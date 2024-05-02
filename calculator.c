a648da8906c0d36f692ff343b3e2b8cc7087b5fb
Suman Rajput <107524156+sumansingh05@users.noreply.github.com>
2024-05-02 16:49:51 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <math.h>
+
+// double division(double a, double b)
+// {
+//     if (b == 0)
+//     {
+//         printf("Invalid argument for Division \n");
+//         printf("Division is not possible");
+//     }
+//     else
+//     {
+//         return a / b;
+//     }
+// }
+
+double modulus(int a, int b)
+{
+    if (b == 0)
+    {
+        printf("Modulus is not possible");
+    }
+    else
+    {
+        return a % b;
+    }
+}
+
+void print_menu()
+{
+    printf("\n \n Welcome to simple calculator \n\n");
+    printf("Choose one of the following operation: \n");
+    printf("1 for addition\n");
+    printf("2 for subtraction\n");
+    printf("3 for multiplication\n");
+    printf("4 for division\n");
+    printf("5 for Modulous\n");
+    printf("6 for Power\n");
+    printf("7 for exit\n");
+    printf("Now enter  your choice:");
+}
+
+int main()
+{
+    double first, second, result;
+    int choice;
+
+    while (1)
+    {
+        print_menu();
+
+        // if (scanf("%d", &choice) != 1)
+        // {
+        //     printf("Invalid input. Please enter a number.\n");
+        //     while (getchar() != '\n')
+        //         ; // Clear input buffer
+        //     continue;
+        // }
+
+        scanf("%d",&choice);
+        if (choice == 7)
+        {
+            break;
+        }
+
+        if (choice < 1 || choice > 7)
+        {
+            printf("Invalid choice");
+            continue;
+        }
+
+        printf("Enter first number:");
+        scanf("%lf", &first);
+        printf("Enter second number:");
+        scanf("%lf", &second);
+
+        switch (choice)
+        {
+        case 1: // addition
+            result = first + second;
+            break;
+
+        case 2: // subtraction
+            result = first - second;
+            break;
+
+        case 3: // multiplication
+            result = first * second;
+            break;
+
+        case 4: // division
+            if (second == 0)
+            {
+                printf("Invalid argument for Division \n");
+                printf("Division is not possible");
+                continue;
+            }
+            else
+            {
+                return first / second;
+            }
+            
+            break;
+
+        case 5: // modulus
+            result = modulus(first, second);
+            break;
+
+        case 6:
+            result = pow(first, second);
+            break;
+        }
+        printf("Result: %lf\n", result);
+    };
+
+    return 0;
+}
